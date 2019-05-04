2435cfa4c53190c40ff43c1c410cbcc4fcf16946
Jerome Paddick <jkpaddick@gmail.com>
2019-05-04 18:09:17 +0100

    refactor NeuralNet

--- NeuralNet.py
+++ NeuralNet.py
@@ -11,62 +11,62 @@ def sigmoid(np_input):
     return np.array([[  1 / (1 + np.exp(-z)) for z in np_input   ]])
 
 
-input_arrays = [[[0.05, 0.10], [0.01, 0.99]]]# , [[0.1, 0.05],[0.02, 0.98]]]
+input_arrays = [[[0.05, 0.10], [0.01, 0.99]]]
 
-for x in input_arrays:
-    x[0] = np.array(x[0])
-    x[1] = np.array(x[1])
+def neural_net(input_arrays):
+    for x in input_arrays:
+        x[0] = np.array(x[0])
+        x[1] = np.array(x[1])
 
-runcount = 1
-b1 = 0.35
-b2 = 0.60
-Eta = 0.5  # Learning rate
+    cycles = 1
+    b1 = 0.35
+    b2 = 0.60
+    Eta = 0.5  # Learning rate
 
-length = len(input_arrays[0])
-I_weights = evendist(length)
+    length = len(input_arrays[0])
+    I_weights = evendist(length)
 
-I_weights = np.array([[0.15, 0.25], [0.20, 0.30]])
-H_weights = np.array([[0.40, 0.50], [0.45, 0.55]])
+    I_weights = np.array([[0.15, 0.25], [0.20, 0.30]])
+    H_weights = np.array([[0.40, 0.50], [0.45, 0.55]])
 
-for x in range(runcount):
-    for input_array in input_arrays:
+    for x in range(cycles):
+        for input_array in input_arrays:
 
-        target = input_array[1]
-        fwd_I = np.array([input_array[0]]).T
+            target = input_array[1]
+            fwd_I = np.array([input_array[0]]).T
 
-        # --> FORWARD PASS
-        out_fwd_H = sigmoid((fwd_I * I_weights).sum(axis=0) + b1)
-        out_fwd_O = sigmoid((out_fwd_H * H_weights).sum(axis=0) + b2)
-        # <-- FORWARD PASS
+            # --> FORWARD PASS
+            out_fwd_H = sigmoid((fwd_I * I_weights).sum(axis=0) + b1)
+            out_fwd_O = sigmoid((out_fwd_H * H_weights).sum(axis=0) + b2)
+            # <-- FORWARD PASS
 
-        Delta_O = (-(target-out_fwd_O))*(out_fwd_O*(1-out_fwd_O))
-        Delta_H = (-(target-out_fwd_H))*(out_fwd_H*(1-out_fwd_H))
+            Delta_O = (-(target-out_fwd_O))*(out_fwd_O*(1-out_fwd_O))
+            Delta_H = (-(target-out_fwd_H))*(out_fwd_H*(1-out_fwd_H))
 
-        I_weights = I_weights - Eta * fwd_I*(Delta_O*H_weights).sum(axis=1)*out_fwd_H*(1-out_fwd_H)
-        H_weights = H_weights - Eta * out_fwd_H * Delta_O
+            I_weights = I_weights - Eta * fwd_I*(Delta_O*H_weights).sum(axis=1)*out_fwd_H*(1-out_fwd_H)
+            H_weights = H_weights - Eta * out_fwd_H * Delta_O
 
-# print("I_weights", I_weights)
-# print("H_weights", H_weights)
-print("Final Out:", out_fwd_O)
+    # print("I_weights", I_weights)
+    # print("H_weights", H_weights)
+    print("Final Out:", out_fwd_O)
+    return out_fwd_O
 
-
-
-
-# numpy axis:   down  --> (axis=0) or ndarray[:, 1]
-#               right --> (axis=1) or ndarray[1, :]
-
-# Input = [Ia, Ib, Ic]
-
-#   Ia -< H1 -< Ox
-#   Ib -< H2 -< Oy
-#   Ic -< H3 -< Oz
-#   b1    b2
-
-#   Ia x (Wa1 Wa2 Wa3) = net(H1)
-#   Ib x (Wb1 Wb2 Wb3) = net(H2)
-#   Ic x (Wc1 Wc2 Wc3) = net(H3)
 
 """
+numpy axis:   down  --> (axis=0) or ndarray[:, 1]
+              right --> (axis=1) or ndarray[1, :]
+
+Input = [Ia, Ib, Ic]
+
+  Ia -< H1 -< Ox
+  Ib -< H2 -< Oy
+  Ic -< H3 -< Oz
+  b1    b2
+
+  Ia x (Wa1 Wa2 Wa3) = net(H1)
+  Ib x (Wb1 Wb2 Wb3) = net(H2)
+  Ic x (Wc1 Wc2 Wc3) = net(H3)
+
       O1  O2
        ↓   ↓
 H1 → wa1 wa2
@@ -74,20 +74,9 @@ H2 → wb1 wb2
       t1  t2
 """
 
-# a = np.array([[1, 2, 3]]).T
-# b = np.ones((3,3))
-"""
-   ___               ___
-  |. .|             |. .|
-  |_c_|             |_-_|
->-|X X|-< [ERROR] >-|X X|-<
-   |_|               |_|
-   | |               | |
-   \ \              / /
 
 """
-
-"""
+VERBOSE CODE ( above with working )
 for x in range(runcount):
     for input_array in input_arrays:
 
